4dc750c1962f1bc97ec6f5520aea2b9bea56e88f
Noah Ross <me@noah.dev>
2023-12-17 01:57:45 -0500

    added row numbering

--- src/lib/components/table.svelte
+++ src/lib/components/table.svelte
@@ -35,29 +35,36 @@
 <table class="table-fixed w-full">
 	<thead>
 		<tr>
-			{#each Array.from({ length: 74 }, (_, i) => i) as col}
-				<th class="text-center text-xxs" style="--cell-size: {cellSize}px">{col}</th>
+			{#each Array.from({ length: 75 }, (_, i) => i) as col}
+				<th class="text-center text-xxs" style="--cell-size: {cellSize}px"
+					>{col - 1 != -1 ? col - 1 : ''}</th
+				>
 			{/each}
 		</tr>
 	</thead>
 	<tbody>
 		{#each matrix as row, rowIndex}
-			<tr>
-				{#each row as cell, cellIndex}
-					<td
-						class={`cell p-2 border border-gray-200 text-center ${
-							isImpossibleScore(rowIndex, cellIndex)
-								? 'bg-gray-700'
-								: matrix[rowIndex][cellIndex].count > 0
-									? 'bg-green-700'
-									: 'bg-red-200'
-						}`}
-						style="--cell-size: {cellSize}px"
-					>
-						<!-- Display cell data here -->
-					</td>
-				{/each}
-			</tr>
+			{#if rowIndex <= 51}
+				<tr>
+					<th class="text-center text-xxs" style="--cell-size: {cellSize}px">{rowIndex}</th>
+					<!-- Row label -->
+
+					{#each row as cell, cellIndex}
+						<td
+							class={`cell p-2 border border-gray-200 text-center ${
+								isImpossibleScore(rowIndex, cellIndex)
+									? 'bg-gray-700'
+									: matrix[rowIndex][cellIndex].count > 0
+										? 'bg-green-700'
+										: 'bg-red-200'
+							}`}
+							style="--cell-size: {cellSize}px"
+						>
+							<!-- Display cell data here -->
+						</td>
+					{/each}
+				</tr>
+			{/if}
 		{/each}
 	</tbody>
 </table>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import Table from '$lib/components/table.svelte';
 </script>
 
-<p class="text-center font-extrabold text-8xl py-16">Simple Scorigami</p>
+<p class="text-center font-extrabold text-8xl py-4">Simple Scorigami</p>
 
-<div class="w-5/6 mx-auto">
+<div class="w-5/6 mx-auto pb-8">
 	<Table />
 </div>
